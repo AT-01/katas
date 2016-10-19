2d98835937f94d878e7a9f178f0ce063b0030814
Miguel Aldo Balderrama Vaca <miguel.aldobv@gmail.com>
2016-10-19 16:04:54 -0400

    added BitCounting and AveragesOfNumber class

---
+++ src/main/java/org/fundacionjala/katas/averagesofnumbers/AveragesOfNumbers.java
@@ -0,0 +1,30 @@
+package org.fundacionjala.katas.averagesofnumbers;
+
+/**
+ * Write a method, that gets an array of integer-numbers and return an array of the averages
+ * of each integer-number and his follower, if there is one.
+ */
+final class AveragesOfNumbers {
+
+    /**
+     * Constructor private.
+     */
+    private AveragesOfNumbers() {
+    }
+
+    /**
+     * This method gets an array of integer-numbers and return an array of the averages
+     * of each integer-number and his follower, if there is one.
+     *
+     * @param numbers Array of integer-numbers.
+     * @return and return an array of the averages.
+     */
+    static double[] averages(final int[] numbers) {
+        final double[] res = new double[null == numbers || numbers.length == 0 ? 0 : numbers.length - 1];
+        for (int i = 0; i < res.length; i++) {
+            assert numbers != null;
+            res[i] = (numbers[i] + numbers[i + 1]) / 2.0;
+        }
+        return res;
+    }
+}
---
+++ src/test/java/org/fundacionjala/katas/averagesofnumbers/AveragesOfNumbersTest.java
@@ -0,0 +1,22 @@
+package org.fundacionjala.katas.averagesofnumbers;
+
+import java.util.Arrays;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Test for {@link AveragesOfNumbers}.
+ */
+public class AveragesOfNumbersTest {
+    /**
+     * This method executed basic test.
+     */
+    @Test
+    public void basicTests() {
+        assertEquals(Arrays.toString(new double[]{2, 2, 2, 2}), Arrays.toString(AveragesOfNumbers.averages(new int[]{2, 2, 2, 2, 2})));
+        assertEquals(Arrays.toString(new double[]{0, 0, 0, 0}), Arrays.toString(AveragesOfNumbers.averages(new int[]{2, -2, 2, -2, 2})));
+        assertEquals(Arrays.toString(new double[]{2, 4, 3, -4.5}), Arrays.toString(AveragesOfNumbers.averages(new int[]{1, 3, 5, 1, -10})));
+    }
+}
